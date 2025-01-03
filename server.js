dc5110ee3108ab2d78ab50825510d42c469926c5
neethafrancis26 <neetha.f@coddletech.com>
2025-01-03 14:09:23 +0530

    updatet the folder structure

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+app.disable('x-powered-by');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "hotel_management",
-});
-
-connection.connect();
-
-app.get("/", (req, res) => {
-    connection.query(`SELECT * FROM customers`, function (err, rows, fields) {
-        console.log("Connection result error " + err);
-        console.log("no of records is " + rows.length);
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(rows));
-    });
-});
-
-app.get("/user", (req, res, next) => {
-    res.send("User Router Working");
+const customerRoute = require("./routes/customers");
+app.use(express.json());
+app.use("/customers", customerRoute);
+app.get("/", (req, res, next) => {
+    res.send("Home Page");
 });
 
 app.get("*", (req, res, next) => {
